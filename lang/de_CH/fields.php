b768e768f739224b412c7dbf0a23eff9a3ad5268
Lars Heidkämper <heidkaemper@new-data-services.de>
2026-01-31 11:18:03 +0100

    dont forget the swiss

---
+++ lang/de_CH/fields.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'source.title'             => 'Quelle',
+    'source.instructions'      => 'Der URL-Pfad, von dem weitergeleitet werden soll. Verwende * als Wildcard (z.B. /blog/*).',
+    'destination.title'        => 'Ziel',
+    'destination.instructions' => 'Die Ziel-URL. Verwende $1, $2 usw. für erfasste Wildcards.',
+    'regex.title'              => 'Regex',
+    'regex.instructions'       => 'Unterstützung regulärer Ausdrücke aktivieren (fortgeschritten).',
+    'status_code.title'        => 'Status Code',
+    'status_code.instructions' => 'Wähle 301 für permanente oder 302 für temporäre Weiterleitungen.',
+    'enabled.title'            => 'Aktiviert',
+    'enabled.instructions'     => 'Ob diese Weiterleitung aktiv ist.',
+    'sites.title'              => 'Seiten',
+    'sites.instructions'       => 'Weiterleitung auf bestimmte Seiten beschränken.',
+];
---
+++ lang/de_CH/messages.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    'redirects'                => 'Weiterleitungen',
+    'redirects_intro'          => 'Verwalte URL-Änderungen und halte Besucher auf Kurs.',
+    'create_first_redirect'    => 'Erstelle deine erste Weiterleitung.',
+    'create_redirect'          => 'Weiterleitung erstellen',
+    'edit_redirect'            => 'Weiterleitung bearbeiten',
+    'learn_about_redirects'    => 'Simple Redirects Addon',
+    'redirect_created'         => 'Weiterleitung erstellt',
+    'redirect_saved'           => 'Weiterleitung gespeichert',
+    'redirect_deleted'         => 'Weiterleitung gelöscht',
+    'delete_confirmation'      => 'Möchtest du diese Weiterleitung wirklich löschen?',
+    'redirects_reordered'      => 'Sortierung gespeichert',
+    'order_save_failed'        => 'Sortierung konnte nicht gespeichert werden',
+    'save_failed'              => 'Speichern fehlgeschlagen',
+    'export_csv'               => 'CSV exportieren',
+    'import_csv'               => 'CSV importieren',
+    'import_description'       => 'Importiere Weiterleitungen aus einer CSV-Datei',
+    'required_columns_missing' => 'Erforderliche Spalten (source, destination) fehlen',
+    'import_complete'          => 'Import abgeschlossen',
+    'import_failed'            => 'Import fehlgeschlagen',
+];
